f8527f51626870fc52020b3d0ec36ac72c134ac3
Rohan <rohansibi13@gmail.com>
2026-07-29 12:58:45 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,31 @@
+import os
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+
+class Config:
+
+
+    APP_NAME = "NEXUS"
+
+
+    VERSION = "2.0"
+
+
+    DEBUG = (
+        os.getenv(
+            "DEBUG",
+            "False"
+        )
+        == "True"
+    )
+
+
+    OLLAMA_URL = os.getenv(
+        "OLLAMA_URL",
+        "http://localhost:11434"
+    )
