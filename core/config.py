8439bd2132c02b70a67e071b5bfe9c86c1a867b0
Rohan <rohansibi13@gmail.com>
2026-07-29 13:10:06 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -9,23 +9,24 @@ load_dotenv()
 
 class Config:
 
-
     APP_NAME = "NEXUS"
 
-
     VERSION = "2.0"
-
-
-    DEBUG = (
-        os.getenv(
-            "DEBUG",
-            "False"
-        )
-        == "True"
-    )
 
 
     OLLAMA_URL = os.getenv(
         "OLLAMA_URL",
         "http://localhost:11434"
+    )
+
+
+    OLLAMA_MODEL = os.getenv(
+        "OLLAMA_MODEL",
+        "llama3"
+    )
+
+
+    MEMORY_DB = os.getenv(
+        "MEMORY_DB",
+        "nexus_memory.db"
     )
