019419d1dc80e1633e0bca14efa882f28dd0f98a
Rohan <rohansibi13@gmail.com>
2026-07-29 23:33:54 +0300

    Update test_voice.py

--- test_voice.py
+++ test_voice.py
@@ -7,10 +7,37 @@ engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 
 
-for index, voice in enumerate(voices):
+# Select Hazel
 
-    print(
-        index,
-        voice.name,
-        voice.id
-    )
+for voice in voices:
+
+    if "Hazel" in voice.name:
+
+        engine.setProperty(
+            "voice",
+            voice.id
+        )
+
+        break
+
+
+
+engine.setProperty(
+    "rate",
+    155
+)
+
+
+engine.setProperty(
+    "volume",
+    1.0
+)
+
+
+
+engine.say(
+    "Hello Rohan. I am NEXUS. Voice systems are now online."
+)
+
+
+engine.runAndWait()
