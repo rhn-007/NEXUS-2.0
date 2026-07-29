efe982779680abcbd5c4d9b53d3c0865c43bd04f
Rohan <rohansibi13@gmail.com>
2026-07-29 12:58:31 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,40 @@
+import logging
+import sys
 
+
+def setup_logger(name):
+
+    logger = logging.getLogger(name)
+
+
+    if logger.handlers:
+
+        return logger
+
+
+    logger.setLevel(
+        logging.INFO
+    )
+
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+
+    console = logging.StreamHandler(
+        sys.stdout
+    )
+
+
+    console.setFormatter(
+        formatter
+    )
+
+
+    logger.addHandler(
+        console
+    )
+
+
+    return logger
