259b7383af5c9eda17711a8a45609e30aff4689e
Rohan <rohansibi13@gmail.com>
2026-07-29 12:58:57 +0300

    Update assistant.py

--- core/assistant.py
+++ core/assistant.py
@@ -1,1 +1,40 @@
+from utils.logger import setup_logger
+from core.config import Config
 
+
+
+logger = setup_logger(__name__)
+
+
+
+class NexusAssistant:
+
+
+    def __init__(self):
+
+        self.name = Config.APP_NAME
+
+        self.version = Config.VERSION
+
+
+        logger.info(
+            "NEXUS core initialized"
+        )
+
+
+
+    def start(self):
+
+        logger.info(
+            f"{self.name} {self.version} ready."
+        )
+
+
+        print(
+            "\n🤖 N.E.X.U.S 2.0 Online\n"
+        )
+
+
+        print(
+            "System foundation loaded successfully."
+        )
